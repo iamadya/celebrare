b52933602dfcde8b1cf368ab8261c58b5a9dc20f
Adya Jha <adyajha2002@gmail.com>
2023-11-06 14:06:48 +0530

    final

--- lib/camera_operations/camera_operations.dart
+++ lib/camera_operations/camera_operations.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class CameraOperation extends StatefulWidget {
   final String imagePath;
 
-  const CameraOperation({Key? key, required this.imagePath}) : super(key: key);
+  const CameraOperation({Key? key, required this.imagePath});
 
   @override
   _CameraOperationState createState() => _CameraOperationState();
@@ -15,6 +15,7 @@ class _CameraOperationState extends State<CameraOperation> {
   double _rotation = 0;
   bool _flippedHorizontally = false;
   bool _flippedVertically = false;
+  double _cropBoxSize = 200; // Initial size of the crop box
 
   @override
   void initState() {
@@ -65,7 +66,7 @@ class _CameraOperationState extends State<CameraOperation> {
             onPressed: _flipHorizontally,
           ),
           IconButton(
-            icon: Icon(Icons.flip_camera_android),
+            icon: Icon(Icons.crop_rotate),
             iconSize: 30,
             onPressed: _flipVertically,
           ),
@@ -77,11 +78,58 @@ class _CameraOperationState extends State<CameraOperation> {
           transform: Matrix4.identity()
             ..rotateZ(_rotation * (3.1415926535897932 / 180))
             ..scale(_flippedHorizontally ? -1.0 : 1.0, _flippedVertically ? -1.0 : 1.0),
-          child: Image.file(
-            _imageFile!,
-            height: 400,
-            width: 400,
+          child: Stack(
+            children: [
+              Image.file(
+                _imageFile!,
+                height: 400,
+                width: 400,
+              ),
+              Positioned(
+                top: (400 - _cropBoxSize) / 2,
+                left: (400 - _cropBoxSize) / 2,
+                child: CropBox(
+                  size: _cropBoxSize,
+                  onSizeChanged: (newSize) {
+                    setState(() {
+                      _cropBoxSize = newSize;
+                    });
+                  },
+                ),
+              ),
+            ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CropBox extends StatelessWidget {
+  final double size;
+  final ValueChanged<double> onSizeChanged;
+
+  const CropBox({
+    required this.size,
+    required this.onSizeChanged,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onScaleStart: (details) {
+        onSizeChanged(size);
+      },
+      onScaleUpdate: (details) {
+        double newSize = size * details.scale;
+        onSizeChanged(newSize.clamp(100.0, 400.0)); // Limit the minimum and maximum size
+      },
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
         ),
       ),
     );
